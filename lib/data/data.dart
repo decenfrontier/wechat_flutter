9f6b28d5a78e95a3d159f3e0597fe2a2369179b6
wangshuang <ws156858@163.com>
2022-03-20 13:13:03 +0800

    feat: 聊天列表

---
+++ lib/data/data.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class Data {
+  static List message = [
+    {
+      "avator": "assets/images/avator.jpg",
+      "nickName": "阿布",
+      "time": "08:56",
+      "lastMessage": "[图片]"
+    },
+    {
+      "avator": "assets/images/file.jpg",
+      "nickName": "文件传输助手",
+      "time": "09:55",
+      "lastMessage": "[图片]"
+    },
+    {
+      "avator": "assets/images/506.jpg",
+      "nickName": "云中君",
+      "time": "10:25",
+      "lastMessage": "记忆触手可得，蓝天延绵不绝"
+    },
+    {
+      "avator": "assets/images/icon.png",
+      "nickName": "微信团队",
+      "time": "11:24",
+      "lastMessage": "[图片]"
+    }
+  ];
+
+  static Map contact = {
+    "first":[],
+    "A": [
+      {"avator": "assets/images/avator.jpg", "nickName": "阿布", "pinyin": "abu"}
+    ],
+    "L": [
+      {
+        "avator": "assets/images/avator2.jpg",
+        "nickName": "老葛",
+        "pinyin": "luokeke"
+      }
+    ],
+    "W": [
+      {
+        "avator": "assets/images/file.jpg",
+        "nickName": "文件传输助手",
+        "pinyin": "wenjianchuanshuzhushou"
+      },
+      {
+        "avator": "assets/images/icon.png",
+        "nickName": "微信团队",
+        "pinyin": "weixintuandui"
+      },
+      {
+        "avator": "assets/images/file.jpg",
+        "nickName": "文件传输助手",
+        "pinyin": "wenjianchuanshuzhushou"
+      },
+      {
+        "avator": "assets/images/icon.png",
+        "nickName": "微信团队",
+        "pinyin": "weixintuandui"
+      },
+      {
+        "avator": "assets/images/file.jpg",
+        "nickName": "文件传输助手",
+        "pinyin": "wenjianchuanshuzhushou"
+      },
+      {
+        "avator": "assets/images/icon.png",
+        "nickName": "微信团队",
+        "pinyin": "weixintuandui"
+      },
+      {
+        "avator": "assets/images/file.jpg",
+        "nickName": "文件传输助手",
+        "pinyin": "wenjianchuanshuzhushou"
+      },
+      {
+        "avator": "assets/images/icon.png",
+        "nickName": "微信团队",
+        "pinyin": "weixintuandui"
+      },
+      {
+        "avator": "assets/images/file.jpg",
+        "nickName": "文件传输助手",
+        "pinyin": "wenjianchuanshuzhushou"
+      },
+      {
+        "avator": "assets/images/icon.png",
+        "nickName": "微信团队",
+        "pinyin": "weixintuandui"
+      },
+      {
+        "avator": "assets/images/file.jpg",
+        "nickName": "文件传输助手",
+        "pinyin": "wenjianchuanshuzhushou"
+      },
+      {
+        "avator": "assets/images/icon.png",
+        "nickName": "微信团队",
+        "pinyin": "weixintuandui"
+      },
+    ]
+  };
+}
--- lib/page/Message.dart
+++ lib/page/Message.dart
@@ -1,81 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ws_chat_flutter/data/data.dart';
 
 class message extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery
+        .of(context)
+        .size;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
         shadowColor: Colors.grey.shade300,
         backgroundColor: Colors.grey.shade300,
-        title: Text("微信", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        title: Text(
+          "微信",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
         actions: [
-          Image.asset("assets/images/search.png", width: 25,),
-          SizedBox(width: 10,),
-          Image.asset("assets/images/more.png", width: 25,),
-          SizedBox(width: 10,),
+          Image.asset(
+            "assets/images/search.png",
+            width: 25,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Image.asset(
+            "assets/images/more.png",
+            width: 25,
+          ),
+          SizedBox(
+            width: 10,
+          ),
         ],
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarBrightness: Brightness.dark,
           statusBarColor: Colors.grey.shade300,
         ),
       ),
-
-    );
-  }
-
-  Padding MessageItem(
-      String avater, String nickName, String time, String lastMesaage) {
-    return Padding(
-      padding: EdgeInsets.only(top: 10,bottom: 5 ,left: 17,right: 15),
-      child: Row(
-        children: [
-          Container(
-            child: ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-              child: Image.asset(
-                avater,
-                width: 50,
-              ),
-            ),
-          ),
-          Expanded(
-              child: Container(
-                margin: EdgeInsets.only(left: 10),
-                decoration: BoxDecoration(
-                    border: Border(
-                        bottom:
-                        BorderSide(color: Colors.grey.shade300, width: 0.5))),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          nickName,
-                          style: TextStyle(color: Colors.black, fontSize: 18),
-                        ),
-                        Text(
-                          time,
-                          style: TextStyle(color: Colors.grey, fontSize: 14),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 5),
-                    Text(lastMesaage,
-                        style: TextStyle(color: Colors.grey, fontSize: 14)),
-                    SizedBox(
-                      height: 15,
-                    )
-                  ],
-                ),
-              ))
-        ],
+      body: Container(
+        height: size.height,
+        child: ListView.builder(
+            itemCount: Data.message.length,
+            itemBuilder: (context, index) {
+              Map item = Data.message[index];
+              return MessageItem(item["avator"],item["nickName"],item["time"],item["lastMessage"]);
+            }),
       ),
     );
   }
+}
+
+Container MessageItem(String avator, String name, String time, String lastMessage) {
+  return Container(
+    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+    child: Row(children: [
+      ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        child: Image.asset("$avator", width: 50,),
+      ),
+      SizedBox(width: 10,),
+      Expanded(child: Column(
+          crossAxisAlignment:CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text("$name", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16)),
+                Spacer(),
+                Text("$time", style: TextStyle(color: Colors.grey, fontSize: 13)),
+              ],
+            ),
+            SizedBox(height: 5,),
+            Text("$lastMessage", style: TextStyle(color: Colors.grey, fontSize: 14)),
+            SizedBox(height: 10,),
+            Divider(height: 1, color: Colors.grey.shade300)
+          ]
+      ))
+    ]),
+  );
 }
