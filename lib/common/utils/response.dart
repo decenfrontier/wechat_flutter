1c5d514375787e16d97a58789e3397d1fb913e78
ws <ws156858@163.com>
2022-04-29 17:00:41 +0800

    feat: 返回状态码改为整数

--- lib/common/utils/response.dart
+++ lib/common/utils/response.dart
@@ -1,16 +1,16 @@
 class Ret {
-  static String statusOk = "1000"; 
+  static int statusOk = 0; 
 }
 
 class ResponseData<T> {
-  String code;
+  int code;
   String msg;
   T? data;
 
   ResponseData({required this.code, required this.msg, this.data});
   factory ResponseData.fromJson(Map<String, dynamic> json) =>
       ResponseData(
-        code: json['code'] as String,
+        code: json['code'] as int,
         msg: json['msg'] as String,
         data: json['data'] as T?,
       );
